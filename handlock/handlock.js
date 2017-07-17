312333b0d95c77c72db3ac16f4048967f09ddda5
YongzhiWu <wyz1037@hust.edu.cn>
2017-07-17 11:26:48 +0800

    20170717
    20170717

--- handlock/handlock.js
+++ handlock/handlock.js
@@ -16,6 +16,7 @@ function drawCircle(canvas, x, y, radius, startAngle, endAngle, anticlockwise, c
 		ctx.strokeStyle = color;
 		ctx.stroke();
 	}
+	ctx.closePath();
 	return ctx;
 }
 /**
@@ -27,15 +28,17 @@ function drawCircle(canvas, x, y, radius, startAngle, endAngle, anticlockwise, c
  */
 function drawLine(canvas, xFrom, yFrom, xTo, yTo, color) {
 	var ctx = canvas.getContext("2d");
+	ctx.beginPath();
 	ctx.lineTo(xFrom, yFrom);
 	ctx.moveTo(xTo, yTo);
 	ctx.strokeStyle = color;
 	ctx.stroke();
+	ctx.closePath();
 	return ctx;
 }
 /**
  * @description Add a function which will be executing when the window is onload.
- * @param func The function that is added.
+ * @param func The function that is to add.
  */
 function addLoadEvent(func) {
 	var oldonload = window.onload;
@@ -48,18 +51,82 @@ function addLoadEvent(func) {
 		}
 	}
 }
-
+/**
+ * @description getLocation is a function that get X-coordinate and Y-coordinate on canvas.
+ * @param  canvas The rectangular coordinate system.
+ * @param x, y The coordinates in the window.
+ * @return the coordinate.
+ */
+function getLocation(canvas, x, y) {
+	var bbox = canvas.getBoundingClientRect(); //获取canvas相对于视窗的位置集合
+	return {
+		x: (x - bbox.left) * (canvas.width / bbox.width),
+		y: (y - bbox.top) * (canvas.height / bbox.height)
+	}
+}
+/**
+ * getDistance is a function that calculate the distance of two points.
+ * @return the distance.
+ */
+function getDistance(x1, y1, x2, y2) {
+	return Math.sqrt((x1 - x2) ^ 2 + (y1 - y2) ^ 2);
+}
+/**
+ * whichCircle is a function that find which circle the mouse is near.
+ * @param  location X or Y coordinate.
+ * @param  len The value of width or height.
+ * @return the number of circle.
+ */
+function whichCircle(location, len) {
+	if (location <= 3 * len / 8) {
+		return 0;
+	} else if ((3 * len / 8 < location) && (location <= 5 * len / 8)) {
+		return 1;
+	} else {
+		return 2;
+	}
+}
+/**
+ * clearCanvas is a function that clear the given canvas.
+ */
+function clearCanvas(c) {
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, c.width, c.height);
+}
+/*set the width of canvas*/
 var canvas = document.getElementsByTagName("canvas");
 var handlock = document.getElementById("handlock");
 for (var i = 0; i < canvas.length; i++) {
 	canvas[i].setAttribute("width", handlock.offsetWidth);
 	canvas[i].setAttribute("height", handlock.offsetWidth);
 }
-var c = document.getElementsByTagName("canvas")[2];
-var w = c.getAttribute("width");
-var h = c.getAttribute("height");
+/*draw the first canvas*/
+var w = canvas[0].getAttribute("width");
+var h = canvas[0].getAttribute("height");
 for (var i = 0; i < 3; i++) {
 	for (var j = 0; j < 3; j++) {
-		drawCircle(canvas[0], w * (i + 1) / 4, h * (j + 1) / 4, 10, 0, 2 * Math.PI, false, "#969696", 1);
+		drawCircle(canvas[1], w * (i + 1) / 4, h * (j + 1) / 4, 10, 0, 2 * Math.PI, false, "#969696", 1);
 	}
 }
+/*draw the second canvas*/
+var clicked = new Array(9); //clicked数组元素表示9个点是否已被连接
+clicked.fill(false);
+var flag = false; //flag为false，表示未开始进行连接；flag为true，表示已开始连接点
+canvas[2].addEventListener("mousedown", function(e) {
+	var location = getLocation(canvas[2], e.clientX, e.clientY);
+	var i = whichCircle(location.x, w);
+	var j = whichCircle(location.y, h);
+	if (!clicked[i * 3 + j]) {
+		var distance = getDistance(location.x, location.y, w * (i + 1) / 4, h * (j + 1) / 4);
+		if (distance < 30) {
+			drawCircle(canvas[2], w * (i + 1) / 4, h * (j + 1) / 4, 10, 0, 2 * Math.PI, false, "red", true);
+			drawCircle(canvas[2], w * (i + 1) / 4, h * (j + 1) / 4, 30, 0, 2 * Math.PI, false, "red", false);
+			clicked[i * 3 + j] = true;
+		}
+	}
+});
+canvas[2].addEventListener("mouseup", function() {
+	clearCanvas(canvas[0]);
+	clearCanvas(canvas[2]);
+	clicked.fill(false);
+});
